4ddf64b35ecd0f985fce3097bc4cc6c18a8b23ab
MacimusRex <nagandsoft@gmail.com>
2020-03-20 21:20:25 +0100

    20200320

---
+++ FantasyGameDemo/Assets/Test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] GameObject GO;
+    [SerializeField] int Length, Width;
+
+
+    private void Awake()
+    {
+        SpawnGOss();
+    }
+    public void SpawnGOss()
+    {
+        for (int i = 0; i < Length; i++)
+        {
+            for (int j = 0; j < Width; j++)
+            {
+                
+                Instantiate(GO, new Vector3(i, 0, j), Quaternion.Euler(90,0,0));
+            }
+        }
+    }
+}
